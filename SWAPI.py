5a9f97dcf88537752542a3cf475d6463191fc24d
tonym <iamarrero111@gmail.com>
2022-02-11 00:46:18 -0700

    created swapi script

---
+++ SWAPI.py
@@ -0,0 +1,11 @@
+import requests
+
+starships = requests.get('https://swapi.dev/api/starships').json()
+
+
+for starship in starships["results"]:
+    if starship["pilots"]:
+        print(starship["name"])
+        for pilot in starship["pilots"]:
+            pilots = requests.get(pilot).json()
+            print("  " + pilots["name"])
